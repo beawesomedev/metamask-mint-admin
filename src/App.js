5d2b3b69acd03aad674738ed045fca00658a7e4d
Gourav Singh <gouravsingh@192.168.1.4>
2022-02-04 12:46:40 +0530

    project init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Table, Tag, Space } from "antd";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    fetch("http://metamint-env.eba-hivekhu8.us-east-2.elasticbeanstalk.com/all")
+      .then((response) => response.json())
+      .then((data) => {
+        const dataWithKey = data.map((item, index) => ({
+          ...item,
+          key: item._id,
+        }));
+
+        setData(dataWithKey);
+        setLoading(false);
+        console.log(data);
+      })
+      .catch((error) => {
+        setError(true);
+        setLoading(false);
+      });
+  }, []);
+
+  const whitelistUser = (objectId) => {
+    fetch(
+      `http://metamint-env.eba-hivekhu8.us-east-2.elasticbeanstalk.com/whitelist/${objectId}`,
+      {
+        method: "POST",
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const blacklistUser = (objectId) => {
+    fetch(
+      `http://metamint-env.eba-hivekhu8.us-east-2.elasticbeanstalk.com/blacklist/${objectId}`,
+      {
+        method: "POST",
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+    {
+      title: "Wallet Address",
+      dataIndex: "walletAddress",
+      key: "walletAddress",
+    },
+    {
+      title: "Tags",
+      key: "tags",
+      dataIndex: "tags",
+      render: (tags) => (
+        <>
+          {tags.map((tag) => {
+            let color = tag.length > 5 ? "geekblue" : "green";
+            if (tag === "loser") {
+              color = "volcano";
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (text, record) => (
+        <Space size="middle">
+          <a>Invite {record.name}</a>
+          {!record.isWhiteListed ? (
+            <a onClick={() => whitelistUser(record.key)}>Whitelist</a>
+          ) : (
+            <a onClick={() => blacklistUser(record.key)}>Blacklist</a>
+          )}
+        </Space>
+      ),
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table dataSource={data} columns={columns} />
     </div>
   );
 }
